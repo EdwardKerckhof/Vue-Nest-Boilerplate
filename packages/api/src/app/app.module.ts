8cde590afff5448cfcfc71f9a3841d7f97f99b20
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-08-12 17:09:46 +0200

    add Joi environment variable validation

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from '../auth/auth.module'
 import { UsersModule } from '../users/users.module'
+import Joi from '@hapi/joi'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        DATABASE_ADMIN_EMAIL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        FRONTEND_URL: Joi.string().required(),
+        COOKIE_NAME: Joi.string().required(),
+        PORT: Joi.number()
+      })
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
